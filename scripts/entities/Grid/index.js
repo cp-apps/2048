db4640b4d281bb3ae436227dba5e5835adc6c7df
Isaac H. J. Houngue <hjisaac>
2022-05-27 20:08:36 +0100

    Minor fix

--- scripts/entities/Grid/index.js
+++ scripts/entities/Grid/index.js
@@ -1,5 +1,5 @@
 import { Cell } from "../../entities/index.js";
-import { setCssProps, CssConfigs } from "../../utils/index.js";
+import { setCssProps, CssConfigs, getCoordinates } from "../../utils/index.js";
 import Position from "../Position/index.js";
 import { GRID_DIM } from "../../configs/index.js";
 
@@ -12,11 +12,9 @@ export default class Grid {
     let _element = element;
 
     let _cells = Array.from(element.children, (childElement, childIndex) => {
-      // Note that with $x and $y as the cell position on
-      // the board, " $childIndex = $y * $GRID_DIM + $x ".
+
       const childPosition = new Position(
-        childIndex % GRID_DIM,
-        Math.floor(childIndex / GRID_DIM)
+        ...Object.values(getCoordinates(childIndex))
       );
       return new Cell(childElement, childPosition);
     });
@@ -45,7 +43,12 @@ export default class Grid {
 
   getRandomEmptyCell() {
     const emptyCells = this.getEmptyCells();
-    return emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+    console.log("emptyCells => ", emptyCells)
+    const r = Math.floor(Math.random() * emptyCells.length)
+    console.log('r => ', r)
+    console.log('r => ', getCoordinates(r))
+    return emptyCells[r]
   };
 
 }
